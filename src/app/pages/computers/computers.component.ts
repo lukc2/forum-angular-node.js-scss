cd42cbdd76309ef0ad9e6bd7ecbb7bfb9b614b3b
lukc2 <lukc2@wp.pl>
2021-01-20 10:46:11 +0100

    fg

---
+++ src/app/pages/computers/computers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'computers-cmp',
+    moduleId: module.id,
+    templateUrl: 'computers.component.html'
+})
+
+export class ComputersComponent {
+  postGroup: any;
+}
---
+++ src/app/pages/music/music.component.ts
@@ -0,0 +1,13 @@
+import { Component,OnInit } from '@angular/core';
+
+declare var google: any;
+
+@Component({
+    moduleId: module.id,
+    selector: 'music-cmp',
+    templateUrl: 'music.component.html'
+})
+
+export class MusicComponent {
+  postGroup: any;
+}
