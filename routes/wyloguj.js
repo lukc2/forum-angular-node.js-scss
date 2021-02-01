ff74f86bdd9ce069cfd1d21c0e922ef0ed3b931a
elo240 <61495945+elo240@users.noreply.github.com>
2021-02-01 09:37:46 +0100

    Update wyloguj.js

--- routes/wyloguj.js
+++ routes/wyloguj.js
@@ -2,19 +2,11 @@ const express = require("express");
 const router = express.Router();
 router.get("/", (req, res) => {
 	if (req.session.isLoggedIn == null || req.session.isLoggedIn == false) {
-		res.json({
-			success: false,
-			msg: "Sesja nie istnieje",
-			redirectTo: "/loguj",
-		}).end();
+		res.redirect("/");
 		return;
 	}
 	req.session.destroy((err) => {
-		res.json({
-			success: true,
-			msg: "Pomyślnie wylogowano",
-			redirectTo: "/loguj",
-		});
+		res.redirect("/");
 	});
 });
 module.exports = router;
